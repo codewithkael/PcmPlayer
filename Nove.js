958e5c02fbb06fad1922017b7e5a647a924c0ac1
Kael <codewithkael@gmail.com>
2022-04-18 10:26:25 +0430

    init

--- Nove.js
+++ Nove.js
@@ -1,8 +1,2 @@
 import {NativeModules} from 'react-native';
-const {Nove} = NativeModules;
-
-module.exports = {
-  playAudio: function (rate, byteArray) {
-    Nove.playAudio(rate, byteArray);
-  },
-};
+export const {Nove} = NativeModules;
